b4b8508d20681e3c40b320e6b46ee38c5bb8c30f
Full Stack Laravel | React | Node | Nest | Next | Wordpress Developer <72126136+nabeel244@users.noreply.github.com>
2024-09-30 20:36:40 +0500

    update config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This allows Vite to listen on all available network interfaces (public IP).
+    port: 5174, // The port number you want to run the app on.
+  },
 })
